211ac207870574ec5c56fe2c65387acc5334af84
rainto0322 <rainto0322@foxmail.com>
2025-11-22 23:21:18 +0800

    Deploy site updates

---
+++ js/memo.js
@@ -0,0 +1,93 @@
+class Memo {
+  constructor(options) {
+    this.el = document.querySelector(options.el)
+    this.url = options.url
+    this.img = options.img ? `<img src="${options.img}">` : '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24px" height="30px" fill="#333"><rect x="0" y="0" width="4" height="15"><animate attributeName="opacity" values="1;.2;1" begin="0s" dur="0.6s" repeatCount="indefinite"></animate></rect><rect x="10" y="0" width="4" height="15"><animate attributeName="opacity" values="1;.2;1" begin="0.2s" dur="0.6s" repeatCount="indefinite"></animate></rect><rect x="20" y="0" width="4" height="15"><animate attributeName="opacity" values="1;.2;1" begin="0.4s" dur="0.6s" repeatCount="indefinite"></animate></rect></svg>'
+    this.cpage = 0
+    this.init()
+  }
+
+  $(e) {
+    return this.el.querySelector(e)
+  }
+
+  T(t) {
+    const i = new Date(t)
+    const ps = (p) => { return String(p).padStart(2, "0") }
+    const [e, n, a, s, d] = [i.getFullYear(), ps(i.getMonth() + 1), ps(i.getDate()), ps(i.getHours()), ps(i.getMinutes())];
+    return `${e}-${n}-${a} ${s}:${d}`
+  }
+
+  cr(e) {
+    return document.createElement(e)
+  }
+
+  async init() {
+    const main = `<div id="memo-cont"></div><div id="memo-more" style="text-align:center;padding:2em"></div>`
+    this.el.innerHTML = main
+    this.$('#memo-more').innerHTML = this.img
+    this.FD()
+  }
+
+  async FD() {
+    const reply = await fetch(`${this.url}/memo/li/${this.cpage}/10`)
+
+    if (!reply.ok) {
+      setTimeout(() => {
+        this.el.innerHTML = "•︡ᯅ•︠ Unable connect to radio waves."
+      }, 2000);
+    }
+    this.RD(await reply.json());
+  }
+  RD(params) {
+    const { data, max } = params
+    const fragment = document.createDocumentFragment();
+    for (const item of data) {
+      let { text, img, date } = item
+
+      const memo_item = this.cr('div')
+      memo_item.className = 'm-item'
+
+      let memo_cont = this.cr('div')
+      memo_cont.className = 'i-cont'
+
+      const memo_date = this.cr('time')
+      memo_date.innerHTML = `<div class="i-meta"><time datetime="${date}">${this.T(date)}</time></div>`
+
+      const parser = new DOMParser();
+      const content = parser.parseFromString(text.replace(/\n/g, "<br>"), "text/html").body.innerHTML
+
+      if (img.length >= 1) {
+        memo_cont.innerHTML = `<img src="https://raw.gitcode.com/rainto/Album/raw/main/daily/${img[0]}.png">` + content
+      } else {
+        memo_cont.innerHTML = content
+      }
+
+      memo_item.appendChild(memo_cont)
+      memo_item.appendChild(memo_date)
+      fragment.appendChild(memo_item)
+    }
+
+    const more = this.$('#memo-more')
+
+    if (this.cpage + 1 < max) {
+      const loadMoreButton = this.cr('div');
+      loadMoreButton.textContent = 'ʕ•ᴥ•ʔ Click me to load more.';
+      loadMoreButton.onclick = () => {
+        more.innerHTML = this.img
+        this.loadMore()
+      }
+      more.innerHTML = ''
+      more.appendChild(loadMoreButton);
+    } else {
+      more.innerHTML = "૮₍•́₃•̀₎ა Really not a drop left."
+    }
+
+    this.$('#memo-cont').appendChild(fragment);
+  }
+
+  loadMore() {
+    this.cpage += 1;
+    this.FD();
+  }
+}
